5611f19cab0cad00aec1c22f788ded7e4920bfc4
Jon Lanz <Jon.Lanz@dreamworks.com>
2023-06-14 13:25:54 -0700

    MOONRAY-4952 Add CMake options to set CXX11 ABI and abi-version (#89)
    * Add CMake options to set CXX11 ABI and abi-version
    
    Signed-off-by: James Horsley <jbhorsley@gmail.com>
    
    * Don't define _GLIBCXX_USE_CXX11_ABI
    
    * Add option for -fabi-version
    
    Signed-off-by: James Horsley <jbhorsley@gmail.com>
    Co-authored-by: James Horsley <jbhorsley@gmail.com>

--- cmake/ArrasRenderCompileDefinitions.cmake
+++ cmake/ArrasRenderCompileDefinitions.cmake
@@ -16,7 +16,6 @@ function(ArrasRender_cxx_compile_definitions target)
             >
 
         PUBLIC
-            _GLIBCXX_USE_CXX11_ABI=0                # https://gcc.gnu.org/onlinedocs/libstdc++/manual/using_dual_abi.html
             GL_GLEXT_PROTOTYPES=1                   # This define makes function symbols to be available as extern declarations.
             TBB_SUPPRESS_DEPRECATED_MESSAGES        # Suppress 'deprecated' messages from TBB
     )
