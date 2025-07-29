3399db1cdc8768017f583ba3c793184c77b8d7d0
206519669_nbcuni <jon.lanz@dreamworks.com>
2025-07-29 10:35:11 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -11,7 +11,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '6.15'
+    _version = '6.16'
 
     from rezbuild import earlybind
     return earlybind.version(this, _version)
@@ -40,7 +40,7 @@ requires = [
 
     # MCRT
     'mcrt_dataio-15.10',
-    'moonbase_proxies-14.21',
+    'moonbase_proxies-14.22',
     'mcrt_messages-14.2',
     'scene_rdl2-15.9',
 
