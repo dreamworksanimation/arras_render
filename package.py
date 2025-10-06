6f616edfbac9bf7f6350303dd06e473501bd0c3b
Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>
2025-10-06 16:12:30 -0700

    MOONRAY-5773, MOONRAY-5752 VFX Reference Platform updates (#119)
    * Remove CentOS variants
    
    * Reorder variants so refplat-vfx2023 is variant 0
    
    * Variants 0 and 1 should use the latest oiio
    
    * Remove debug variant to reduce package builds
    
    * Remove oiio-2.3 variant
    
    * Add refplat-vfx2025 variant
    
    * Variants
    
    * Reorder variants

--- package.py
+++ package.py
@@ -24,14 +24,11 @@ help = ('For assistance, '
         "please contact the folio's owner at: psw-jose@dreamworks.com")
 
 variants = [
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2022.0', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'openimageio-2.3.20.0.x', 'gcc-11.x'],
-    ['os-rocky-9', 'opt_level-debug',    'refplat-vfx2023.1', 'openimageio-2.3.20.0.x', 'gcc-11.x'],
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'openimageio-2.4.8.0.x',  'gcc-11.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'openimageio-2.3.20.0.x', 'clang-17.0.6.x'],
-
-    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2022.0', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1'],
-    ['os-CentOS-7', 'opt_level-debug',    'refplat-vfx2022.0', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1'],
+    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'openimageio-2.4.8.0.x',  'clang-17.0.6.x'],
+    # ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2024.0', 'openimageio-2.4.8.0.x',  'gcc-11.x'], # This won't build until we upgrade to Qt6
+    # ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2025.0', 'openimageio-3.0',        'gcc-11.x'], # This won't build until we upgrade to Qt6
+    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2022.0', 'openimageio-2.3.20.0.x', 'gcc-9.3.x.1'],
 ]
 
 requires = [
