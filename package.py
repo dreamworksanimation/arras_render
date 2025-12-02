ec5a7c070a2f895ae927c65294cac290858710f4
206539443_nbcuni <Mike.Day@dreamworks.com>
2025-12-02 12:42:28 -0800

    [skip ci]Bumped minor version for the release.

--- package.py
+++ package.py
@@ -11,7 +11,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '6.29'
+    _version = '6.30'
 
     from rezbuild import earlybind
     return earlybind.version(this, _version)
@@ -74,10 +74,10 @@ requires = [
     'arras4_core-4.10',
 
     # MCRT
-    'mcrt_dataio-15.20',
-    'moonbase_proxies-14.37',
+    'mcrt_dataio-15.21',
+    'moonbase_proxies-14.38',
     'mcrt_messages-14.9',
-    'scene_rdl2-15.19',
+    'scene_rdl2-15.20',
 
     # Third party deps
     'boost',
